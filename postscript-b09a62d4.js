87057a10027360f1df0cf8feef5106250e1741a0
boxi-os <boxi@borkel.net>
2026-09-25 00:13:41 +0200

    Deploy 2026-09-24T22:13:41.860Z

---
+++ postscript-b09a62d4.js
@@ -0,0 +1,14 @@
+await Promise.all([
+  import("./static/scripts/script-0-f1365b3a.js"),
+  import("./static/scripts/script-1-0e41d61b.js"),
+  import("./static/scripts/script-2-f7c52b6a.js"),
+  import("./static/scripts/script-3-5ef560a8.js"),
+  import("./static/scripts/script-4-c0db9ce8.js"),
+  import("./static/scripts/script-5-cc8afa9d.js"),
+  import("./static/scripts/script-6-7e107d7e.js"),
+  import("./static/scripts/script-7-8be19c3a.js"),
+  import("./static/scripts/script-8-632b2822.js"),
+  import("./static/scripts/script-9-c9e900a7.js"),
+  import("./static/scripts/script-10-be15df42.js")
+]);
+await import("./static/scripts/script-11-4a1444dc.js");
